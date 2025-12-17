c64da6073e5bbe689b3ffd97f1cd92d3a04445f7
flurmeng <fleur.meng.23@proton.me>
2025-12-17 02:13:45 -0500

    feat(core): add state functionality to the event module

---
+++ core/events/state/fastapi.go
@@ -0,0 +1,34 @@
+package state
+
+import (
+	"context"
+
+	"github.com/post-quantumqoin/address"
+
+	"github.com/post-quantumqoin/qoin-shor/core/types"
+)
+
+type FastChainApiAPI interface {
+	ChainAPI
+
+	ChainGetTipSet(context.Context, types.TipSetKey) (*types.TipSet, error)
+}
+
+type fastAPI struct {
+	FastChainApiAPI
+}
+
+func WrapFastAPI(api FastChainApiAPI) ChainAPI {
+	return &fastAPI{
+		api,
+	}
+}
+
+func (a *fastAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
+	ts, err := a.FastChainApiAPI.ChainGetTipSet(ctx, tsk)
+	if err != nil {
+		return nil, err
+	}
+
+	return a.FastChainApiAPI.StateGetActor(ctx, actor, ts.Parents())
+}
