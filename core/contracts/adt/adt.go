3ec7a897561e063a3cec80a26e3422e9f7097af5
flurmeng <fleur.meng.23@proton.me>
2025-12-23 05:32:41 -0500

    feat(contracts): add Abstract Data Type

---
+++ core/contracts/adt/adt.go
@@ -0,0 +1,29 @@
+package adt
+
+import (
+	"github.com/ipfs/go-cid"
+
+	"github.com/post-quantumqoin/core-types/abi"
+	"github.com/post-quantumqoin/core-types/cbor"
+)
+
+type Map interface {
+	Root() (cid.Cid, error)
+
+	Put(k abi.Keyer, v cbor.Marshaler) error
+	Get(k abi.Keyer, v cbor.Unmarshaler) (bool, error)
+	Delete(k abi.Keyer) error
+
+	ForEach(v cbor.Unmarshaler, fn func(key string) error) error
+}
+
+type Array interface {
+	Root() (cid.Cid, error)
+
+	Set(idx uint64, v cbor.Marshaler) error
+	Get(idx uint64, v cbor.Unmarshaler) (bool, error)
+	Delete(idx uint64) error
+	Length() uint64
+
+	ForEach(v cbor.Unmarshaler, fn func(idx int64) error) error
+}
