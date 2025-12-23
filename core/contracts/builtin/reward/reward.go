ea1d58060e045b13d81c628da42a233fa99ef103
flurmeng <fleur.meng.23@proton.me>
2025-12-23 05:58:17 -0500

    feat(contracts): add the built-in reward contract

---
+++ core/contracts/builtin/reward/reward.go
@@ -0,0 +1,177 @@
+package reward
+
+import (
+	"github.com/ipfs/go-cid"
+	"golang.org/x/xerrors"
+
+	"github.com/post-quantumqoin/core-types/abi"
+	actorstypes "github.com/post-quantumqoin/core-types/actors"
+	builtin13 "github.com/post-quantumqoin/core-types/builtin"
+	"github.com/post-quantumqoin/core-types/cbor"
+	"github.com/post-quantumqoin/core-types/manifest"
+	builtin0 "github.com/post-quantumqoin/specs-contracts/contracts/builtin"
+	builtin2 "github.com/post-quantumqoin/specs-contracts/contracts/builtin"
+	builtin3 "github.com/post-quantumqoin/specs-contracts/contracts/builtin"
+	builtin4 "github.com/post-quantumqoin/specs-contracts/contracts/builtin"
+	builtin5 "github.com/post-quantumqoin/specs-contracts/contracts/builtin"
+	builtin6 "github.com/post-quantumqoin/specs-contracts/contracts/builtin"
+	builtin7 "github.com/post-quantumqoin/specs-contracts/contracts/builtin"
+	reward0 "github.com/post-quantumqoin/specs-contracts/contracts/builtin/reward"
+
+	actors "github.com/post-quantumqoin/qoin-shor/core/contracts"
+	"github.com/post-quantumqoin/qoin-shor/core/contracts/adt"
+	"github.com/post-quantumqoin/qoin-shor/core/contracts/builtin"
+	"github.com/post-quantumqoin/qoin-shor/core/types"
+)
+
+var (
+	Address = builtin13.RewardActorAddr
+	Methods = builtin13.MethodsReward
+)
+
+func Load(store adt.Store, act *types.Actor) (State, error) {
+	if name, av, ok := actors.GetActorMetaByCode(act.Code); ok {
+		if name != manifest.RewardKey {
+			return nil, xerrors.Errorf("actor code is not reward: %s", name)
+		}
+
+		switch av {
+
+		case actorstypes.Version8:
+			return load8(store, act.Head)
+
+		case actorstypes.Version9:
+			return load9(store, act.Head)
+
+		case actorstypes.Version10:
+			return load10(store, act.Head)
+
+		case actorstypes.Version11:
+			return load11(store, act.Head)
+
+		case actorstypes.Version12:
+			return load12(store, act.Head)
+
+		case actorstypes.Version13:
+			return load13(store, act.Head)
+
+		}
+	}
+
+	switch act.Code {
+
+	case builtin0.RewardActorCodeID:
+		return load0(store, act.Head)
+
+	case builtin2.RewardActorCodeID:
+		return load2(store, act.Head)
+
+	case builtin3.RewardActorCodeID:
+		return load3(store, act.Head)
+
+	case builtin4.RewardActorCodeID:
+		return load4(store, act.Head)
+
+	case builtin5.RewardActorCodeID:
+		return load5(store, act.Head)
+
+	case builtin6.RewardActorCodeID:
+		return load6(store, act.Head)
+
+	case builtin7.RewardActorCodeID:
+		return load7(store, act.Head)
+
+	}
+
+	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
+}
+
+func MakeState(store adt.Store, av actorstypes.Version, currRealizedPower abi.StoragePower) (State, error) {
+	switch av {
+
+	case actorstypes.Version0:
+		return make0(store, currRealizedPower)
+
+	case actorstypes.Version2:
+		return make2(store, currRealizedPower)
+
+	case actorstypes.Version3:
+		return make3(store, currRealizedPower)
+
+	case actorstypes.Version4:
+		return make4(store, currRealizedPower)
+
+	case actorstypes.Version5:
+		return make5(store, currRealizedPower)
+
+	case actorstypes.Version6:
+		return make6(store, currRealizedPower)
+
+	case actorstypes.Version7:
+		return make7(store, currRealizedPower)
+
+	case actorstypes.Version8:
+		return make8(store, currRealizedPower)
+
+	case actorstypes.Version9:
+		return make9(store, currRealizedPower)
+
+	case actorstypes.Version10:
+		return make10(store, currRealizedPower)
+
+	case actorstypes.Version11:
+		return make11(store, currRealizedPower)
+
+	case actorstypes.Version12:
+		return make12(store, currRealizedPower)
+
+	case actorstypes.Version13:
+		return make13(store, currRealizedPower)
+
+	}
+	return nil, xerrors.Errorf("unknown actor version %d", av)
+}
+
+type State interface {
+	cbor.Marshaler
+
+	Code() cid.Cid
+	ActorKey() string
+	ActorVersion() actorstypes.Version
+
+	ThisEpochBaselinePower() (abi.StoragePower, error)
+	ThisEpochReward() (abi.StoragePower, error)
+	ThisEpochRewardSmoothed() (builtin.FilterEstimate, error)
+
+	EffectiveBaselinePower() (abi.StoragePower, error)
+	EffectiveNetworkTime() (abi.ChainEpoch, error)
+
+	TotalStoragePowerReward() (abi.TokenAmount, error)
+
+	CumsumBaseline() (abi.StoragePower, error)
+	CumsumRealized() (abi.StoragePower, error)
+
+	InitialPledgeForPower(abi.StoragePower, abi.TokenAmount, *builtin.FilterEstimate, abi.TokenAmount) (abi.TokenAmount, error)
+	PreCommitDepositForPower(builtin.FilterEstimate, abi.StoragePower) (abi.TokenAmount, error)
+	GetState() interface{}
+}
+
+type AwardBlockRewardParams = reward0.AwardBlockRewardParams
+
+func AllCodes() []cid.Cid {
+	return []cid.Cid{
+		(&state0{}).Code(),
+		(&state2{}).Code(),
+		(&state3{}).Code(),
+		(&state4{}).Code(),
+		(&state5{}).Code(),
+		(&state6{}).Code(),
+		(&state7{}).Code(),
+		(&state8{}).Code(),
+		(&state9{}).Code(),
+		(&state10{}).Code(),
+		(&state11{}).Code(),
+		(&state12{}).Code(),
+		(&state13{}).Code(),
+	}
+}
