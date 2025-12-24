efd1ff6b62e50e6522322f9a6d10b641b697d0ca
flurmeng <fleur.meng.23@proton.me>
2025-12-24 02:27:17 -0500

    feat(core): implement the ledger and remote wallets

---
+++ core/wallet/multi.go
@@ -0,0 +1,179 @@
+package wallet
+
+import (
+	"context"
+
+	"go.uber.org/fx"
+	"go.uber.org/multierr"
+	"golang.org/x/xerrors"
+
+	"github.com/post-quantumqoin/address"
+	"github.com/post-quantumqoin/core-types/crypto"
+
+	"github.com/post-quantumqoin/qoin-shor/api"
+	"github.com/post-quantumqoin/qoin-shor/core/types"
+	ledgerwallet "github.com/post-quantumqoin/qoin-shor/core/wallet/ledger"
+	"github.com/post-quantumqoin/qoin-shor/core/wallet/pqcwallet"
+	"github.com/post-quantumqoin/qoin-shor/core/wallet/remotewallet"
+)
+
+type MultiWallet struct {
+	fx.In // "constructed" with fx.In instead of normal constructor
+
+	Pqc    *pqcwallet.PqcWallet       `optional:"true"`
+	Local  *LocalWallet               `optional:"true"`
+	Remote *remotewallet.RemoteWallet `optional:"true"`
+	Ledger *ledgerwallet.LedgerWallet `optional:"true"`
+}
+
+type getif interface {
+	api.Wallet
+
+	// workaround for the fact that iface(*struct(nil)) != nil
+	Get() api.Wallet
+}
+
+func firstNonNil(wallets ...getif) api.Wallet {
+	for _, w := range wallets {
+		if w.Get() != nil {
+			return w
+		}
+	}
+
+	return nil
+}
+
+func nonNil(wallets ...getif) []api.Wallet {
+	var out []api.Wallet
+	for _, w := range wallets {
+		if w.Get() == nil {
+			continue
+		}
+
+		out = append(out, w)
+	}
+
+	return out
+}
+
+func (m MultiWallet) find(ctx context.Context, address address.Address, wallets ...getif) (api.Wallet, error) {
+	ws := nonNil(wallets...)
+
+	var merr error
+
+	for _, w := range ws {
+		have, err := w.WalletHas(ctx, address)
+		merr = multierr.Append(merr, err)
+
+		if err == nil && have {
+			return w, nil
+		}
+	}
+
+	return nil, merr
+}
+
+func (m MultiWallet) WalletNew(ctx context.Context, keyType types.KeyType) (address.Address, error) {
+	var local getif = m.Pqc
+	if keyType == types.KTSecp256k1Ledger {
+		local = m.Ledger
+	}
+	if keyType == types.KTBLS || keyType == types.KTSecp256k1 {
+		local = m.Local
+	}
+	log.Info("MultiWallet WalletNew")
+	// return m.Pqc.WalletNew(ctx, keyType)
+	w := firstNonNil(m.Remote, local)
+	if w == nil {
+		return address.Undef, xerrors.Errorf("no wallet backends supporting key type: %s", keyType)
+	}
+
+	return w.WalletNew(ctx, keyType)
+}
+
+func (m MultiWallet) WalletHas(ctx context.Context, address address.Address) (bool, error) {
+	w, err := m.find(ctx, address, m.Remote, m.Ledger, m.Local, m.Pqc)
+	return w != nil, err
+}
+
+func (m MultiWallet) WalletList(ctx context.Context) ([]address.Address, error) {
+	out := make([]address.Address, 0)
+	seen := map[address.Address]struct{}{}
+
+	ws := nonNil(m.Pqc, m.Remote, m.Ledger, m.Local)
+	for _, w := range ws {
+		l, err := w.WalletList(ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, a := range l {
+			if _, ok := seen[a]; ok {
+				continue
+			}
+			seen[a] = struct{}{}
+
+			out = append(out, a)
+		}
+	}
+
+	return out, nil
+}
+
+func (m MultiWallet) WalletSign(ctx context.Context, signer address.Address, toSign []byte, meta api.MsgMeta) (*crypto.Signature, error) {
+	w, err := m.find(ctx, signer, m.Remote, m.Pqc, m.Ledger, m.Local)
+	if err != nil {
+		return nil, err
+	}
+	if w == nil {
+		return nil, xerrors.Errorf("key not found for %s", signer)
+	}
+
+	return w.WalletSign(ctx, signer, toSign, meta)
+}
+
+func (m MultiWallet) WalletExport(ctx context.Context, addr address.Address) (*types.KeyInfo, error) {
+	w, err := m.find(ctx, addr, m.Remote, m.Local, m.Pqc)
+	if err != nil {
+		return nil, err
+	}
+	if w == nil {
+		return nil, xerrors.Errorf("key not found for %s", addr)
+	}
+
+	return w.WalletExport(ctx, addr)
+}
+
+func (m MultiWallet) WalletImport(ctx context.Context, info *types.KeyInfo) (address.Address, error) {
+	var local getif = m.Pqc
+	if info.Type == types.KTSecp256k1Ledger {
+		local = m.Ledger
+	}
+	if info.Type == types.KTBLS || info.Type == types.KTSecp256k1 {
+		local = m.Local
+	}
+	w := firstNonNil(m.Remote, local)
+	if w == nil {
+		return address.Undef, xerrors.Errorf("no wallet backends configured")
+	}
+
+	return w.WalletImport(ctx, info)
+}
+
+func (m MultiWallet) WalletDelete(ctx context.Context, address address.Address) error {
+	for {
+		w, err := m.find(ctx, address, m.Remote, m.Pqc, m.Ledger, m.Local)
+		if err != nil {
+			return err
+		}
+		if w == nil {
+			return nil
+		}
+
+		if err := w.WalletDelete(ctx, address); err != nil {
+			return err
+		}
+	}
+}
+
+var _ api.Wallet = MultiWallet{}
