95a4102a6fc542b7040dbd0b4861c8b956774d6e
flurmeng <fleur.meng.23@proton.me>
2025-12-24 02:01:45 -0500

    feat(core): add init and system to genesis in the gen module

---
+++ core/gen/genesis/f01_init.go
@@ -0,0 +1,190 @@
+package genesis
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	cbor "github.com/ipfs/go-ipld-cbor"
+	cbg "github.com/whyrusleeping/cbor-gen"
+	"golang.org/x/xerrors"
+
+	"github.com/post-quantumqoin/address"
+	"github.com/post-quantumqoin/core-types/abi"
+	actorstypes "github.com/post-quantumqoin/core-types/actors"
+	"github.com/post-quantumqoin/core-types/big"
+	"github.com/post-quantumqoin/core-types/manifest"
+	"github.com/post-quantumqoin/specs-contracts/contracts/util/adt"
+
+	bstore "github.com/post-quantumqoin/qoin-shor/blockstore"
+	"github.com/post-quantumqoin/qoin-shor/core/actors"
+	init_ "github.com/post-quantumqoin/qoin-shor/core/actors/builtin/init"
+	"github.com/post-quantumqoin/qoin-shor/core/types"
+	"github.com/post-quantumqoin/qoin-shor/genesis"
+)
+
+func SetupInitActor(ctx context.Context, bs bstore.Blockstore, netname string, initialActors []genesis.Actor, rootVerifier genesis.Actor, remainder genesis.Actor, av actorstypes.Version) (int64, *types.Actor, map[address.Address]address.Address, error) {
+	if len(initialActors) > MaxAccounts {
+		return 0, nil, nil, xerrors.New("too many initial actors")
+	}
+
+	cst := cbor.NewCborStore(bs)
+	ist, err := init_.MakeState(adt.WrapStore(ctx, cst), av, netname)
+	if err != nil {
+		return 0, nil, nil, err
+	}
+
+	if err = ist.SetNextID(MinerStart); err != nil {
+		return 0, nil, nil, err
+	}
+
+	amap, err := ist.AddressMap()
+	if err != nil {
+		return 0, nil, nil, err
+	}
+
+	keyToId := map[address.Address]address.Address{}
+	counter := int64(AccountStart)
+
+	for _, a := range initialActors {
+		if a.Type == genesis.TMultisig {
+			var ainfo genesis.MultisigMeta
+			if err := json.Unmarshal(a.Meta, &ainfo); err != nil {
+				return 0, nil, nil, xerrors.Errorf("unmarshaling account meta: %w", err)
+			}
+			for _, e := range ainfo.Signers {
+
+				if _, ok := keyToId[e]; ok {
+					continue
+				}
+
+				fmt.Printf("init set %s t0%d\n", e, counter)
+
+				value := cbg.CborInt(counter)
+				if err := amap.Put(abi.AddrKey(e), &value); err != nil {
+					return 0, nil, nil, err
+				}
+				counter = counter + 1
+				var err error
+				keyToId[e], err = address.NewIDAddress(uint64(value))
+				if err != nil {
+					return 0, nil, nil, err
+				}
+
+			}
+			// Need to add actors for all multisigs too
+			continue
+		}
+
+		if a.Type != genesis.TAccount {
+			return 0, nil, nil, xerrors.Errorf("unsupported account type: %s", a.Type)
+		}
+
+		var ainfo genesis.AccountMeta
+		if err := json.Unmarshal(a.Meta, &ainfo); err != nil {
+			return 0, nil, nil, xerrors.Errorf("unmarshaling account meta: %w", err)
+		}
+
+		fmt.Printf("init set %s t0%d\n", ainfo.Owner, counter)
+
+		value := cbg.CborInt(counter)
+		if err := amap.Put(abi.AddrKey(ainfo.Owner), &value); err != nil {
+			return 0, nil, nil, err
+		}
+		counter = counter + 1
+
+		var err error
+		keyToId[ainfo.Owner], err = address.NewIDAddress(uint64(value))
+		if err != nil {
+			return 0, nil, nil, err
+		}
+	}
+
+	setupMsig := func(meta json.RawMessage) error {
+		var ainfo genesis.MultisigMeta
+		if err := json.Unmarshal(meta, &ainfo); err != nil {
+			return xerrors.Errorf("unmarshaling account meta: %w", err)
+		}
+		for _, e := range ainfo.Signers {
+			if _, ok := keyToId[e]; ok {
+				continue
+			}
+			fmt.Printf("init set %s t0%d\n", e, counter)
+
+			value := cbg.CborInt(counter)
+			if err := amap.Put(abi.AddrKey(e), &value); err != nil {
+				return err
+			}
+			counter = counter + 1
+			var err error
+			keyToId[e], err = address.NewIDAddress(uint64(value))
+			if err != nil {
+				return err
+			}
+
+		}
+
+		return nil
+	}
+
+	if rootVerifier.Type == genesis.TAccount {
+		var ainfo genesis.AccountMeta
+		if err := json.Unmarshal(rootVerifier.Meta, &ainfo); err != nil {
+			return 0, nil, nil, xerrors.Errorf("unmarshaling account meta: %w", err)
+		}
+		value := cbg.CborInt(80)
+		if err := amap.Put(abi.AddrKey(ainfo.Owner), &value); err != nil {
+			return 0, nil, nil, err
+		}
+	} else if rootVerifier.Type == genesis.TMultisig {
+		err := setupMsig(rootVerifier.Meta)
+		if err != nil {
+			return 0, nil, nil, xerrors.Errorf("setting up root verifier msig: %w", err)
+		}
+	}
+
+	if remainder.Type == genesis.TAccount {
+		var ainfo genesis.AccountMeta
+		if err := json.Unmarshal(remainder.Meta, &ainfo); err != nil {
+			return 0, nil, nil, xerrors.Errorf("unmarshaling account meta: %w", err)
+		}
+
+		// TODO: Use builtin.ReserveAddress...
+		value := cbg.CborInt(90)
+		if err := amap.Put(abi.AddrKey(ainfo.Owner), &value); err != nil {
+			return 0, nil, nil, err
+		}
+	} else if remainder.Type == genesis.TMultisig {
+		err := setupMsig(remainder.Meta)
+		if err != nil {
+			return 0, nil, nil, xerrors.Errorf("setting up remainder msig: %w", err)
+		}
+	}
+
+	amapaddr, err := amap.Root()
+	if err != nil {
+		return 0, nil, nil, err
+	}
+
+	if err = ist.SetAddressMap(amapaddr); err != nil {
+		return 0, nil, nil, err
+	}
+
+	statecid, err := cst.Put(ctx, ist.GetState())
+	if err != nil {
+		return 0, nil, nil, err
+	}
+
+	actcid, ok := actors.GetActorCodeID(av, manifest.InitKey)
+	if !ok {
+		return 0, nil, nil, xerrors.Errorf("failed to get init actor code ID for actors version %d", av)
+	}
+
+	act := &types.Actor{
+		Code:    actcid,
+		Head:    statecid,
+		Balance: big.Zero(),
+	}
+
+	return counter, act, keyToId, nil
+}
