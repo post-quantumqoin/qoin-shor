7b2584cd89d4990789749bd494e5b2798c508e60
flurmeng <fleur.meng.23@proton.me>
2026-01-12 07:43:15 -0500

    feat(node): implement basic services for node

---
+++ node/impl/market/market.go
@@ -0,0 +1,60 @@
+package market
+
+import (
+	"context"
+
+	"github.com/ipfs/go-cid"
+	"go.uber.org/fx"
+
+	"github.com/post-quantumqoin/address"
+
+	actors "github.com/post-quantumqoin/qoin-shor/core/contracts"
+	marketactor "github.com/post-quantumqoin/qoin-shor/core/contracts/builtin/market"
+	"github.com/post-quantumqoin/qoin-shor/core/market"
+	"github.com/post-quantumqoin/qoin-shor/core/types"
+	"github.com/post-quantumqoin/qoin-shor/node/impl/full"
+)
+
+type MarketAPI struct {
+	fx.In
+
+	full.MpoolAPI
+	FMgr *market.FundManager
+}
+
+func (a *MarketAPI) MarketAddBalance(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {
+	params, err := actors.SerializeParams(&addr)
+	if err != nil {
+		return cid.Undef, err
+	}
+
+	smsg, aerr := a.MpoolPushMessage(ctx, &types.Message{
+		To:     marketactor.Address,
+		From:   wallet,
+		Value:  amt,
+		Method: marketactor.Methods.AddBalance,
+		Params: params,
+	}, nil)
+
+	if aerr != nil {
+		return cid.Undef, aerr
+	}
+
+	return smsg.Cid(), nil
+}
+
+func (a *MarketAPI) MarketGetReserved(ctx context.Context, addr address.Address) (types.BigInt, error) {
+	return a.FMgr.GetReserved(addr), nil
+}
+
+func (a *MarketAPI) MarketReserveFunds(ctx context.Context, wallet address.Address, addr address.Address, amt types.BigInt) (cid.Cid, error) {
+	return a.FMgr.Reserve(ctx, wallet, addr, amt)
+}
+
+func (a *MarketAPI) MarketReleaseFunds(ctx context.Context, addr address.Address, amt types.BigInt) error {
+	return a.FMgr.Release(addr, amt)
+}
+
+func (a *MarketAPI) MarketWithdraw(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {
+	return a.FMgr.Withdraw(ctx, wallet, addr, amt)
+}
