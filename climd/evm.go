d15f2ba9c622a89d24e3cc04a5c0f81eaf4ce3f6
flurmeng <fleur.meng.23@proton.me>
2026-03-30 06:09:49 -0400

    feat(climd): add some EVM commands.

--- climd/evm.go
+++ climd/evm.go
@@ -301,10 +301,10 @@ var EvmDeployCmd = &cli.Command{
 
 		delegated, err := ea.ToFilecoinAddress()
 		if err != nil {
-			return fmt.Errorf("failed to calculate f4 address: %w", err)
+			return fmt.Errorf("failed to calculate Q4 Address: %w", err)
 		}
 
-		afmt.Printf("f4 Address: %s\n", delegated)
+		afmt.Printf("Q4 Address: %s\n", delegated)
 
 		if len(wait.Receipt.Return) > 0 {
 			result := base64.StdEncoding.EncodeToString(wait.Receipt.Return)
